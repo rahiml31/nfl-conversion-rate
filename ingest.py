c39ba67eb572b15a949f0d15de388803795ae9c2
Rahim <rahim.lalani31@gmail.com>
2020-12-16 13:51:19 -0600

    Moving everything to one file

--- ingest.py
+++ ingest.py
@@ -4,7 +4,6 @@ import json
 import sqlite3
 
 def main(args):
-
     try:
         # Parse out the command-line arguments #
         props_path = args[1]
@@ -17,20 +16,33 @@ def main(args):
         # Read in the data #
         data = pd.read_csv(data_path)
 
-        # Clean the data and get the necessary columns only #
-        cleaned_data0 = data[props['data_columns']]
-        cleaned_data1 = cleaned_data0[cleaned_data0.Down.eq(3)]
-
-        # Push data to SQL table(sqllite3) #
+        # Push data to SQL table #
         print('Ingesting cleaned data to SQL table.')
         connection = sqlite3.connect(props['database_name'])
-        cleaned_data1.to_sql(props['table_name'], connection, index=True)
-    except:
-        print('There was an issue ingesting the data into the SQL table.')
+        data.to_sql(props['table_name'], connection, index=True)
+        print('Finished ingeting the data.')
+
+        # Read data from SQL table #
+        raw_data = pd.read_sql('select * from play_data;', connection)
+
+        # Clean the data and get the necessary columns only #
+        cleaned_data0 = raw_data[props['data_columns']]
+        cleaned_data1 = cleaned_data0[cleaned_data0.Down.eq(3)]
+        print(cleaned_data1.head(10))
+
+        # Process data to get necessary information #
+        short = cleaned_data1[cleaned_data1.ToGo.lt(3)]
+        medium = cleaned_data1[cleaned_data1.ToGo.between(4, 7)]
+        long = cleaned_data1[cleaned_data1.ToGo.gt(8)]
+        
+
+        # Graph the data(matplotlib) #
+
+    except Exception as e:
+        print('There was an issue ingesting the data into the SQL table.', e)
     else:
-        # Exit job when finished without error #
-        print('Finished ingeting the data, exiting now.')
         sys.exit(0)    
+
 
 if __name__ == '__main__':
     main(sys.argv)
